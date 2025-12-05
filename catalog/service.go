43583573d9cf0475bd9cd817659567ffee6a84ac
douglas_barraza <douglas.barraza@externos-cl.cencosud.com>
2025-12-05 12:27:15 +0000

    add catalog products

---
+++ catalog/service.go
@@ -0,0 +1,61 @@
+package catalog
+
+import (
+	"context"
+
+	"github.com/segmentio/ksuid"
+)
+
+type Service interface {
+	PostProduct(ctx context.Context, name string, description string, price float64) (*Product, error)
+	GetProductById(ctx context.Context, id string) (*Product, error)
+	ListProducts(ctx context.Context, skip, take uint64) ([]Product, error)
+	ListProductsWithIds(ctx context.Context, ids []string) ([]Product, error)
+	SearchProducts(ctx context.Context, query string, skip, take uint64) ([]Product, error)
+}
+
+type Product struct {
+	ID          string  `json:"id"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Price       float64 `json:"price"`
+}
+
+type catalogService struct {
+	repository Repository
+}
+
+func NewService(r Repository) Service {
+	return &catalogService{r}
+}
+
+func (s *catalogService) PostProduct(ctx context.Context, name string, description string, price float64) (*Product, error) {
+	p := &Product{
+		Name:        name,
+		Description: description,
+		Price:       price,
+		ID:          ksuid.New().String(),
+	}
+	if err := s.repository.PutProduct(ctx, *p); err != nil {
+		return nil, err
+	}
+	return p, nil
+}
+func (s *catalogService) GetProductById(ctx context.Context, id string) (*Product, error) {
+	return s.repository.GetProductById(ctx, id)
+}
+func (s *catalogService) ListProducts(ctx context.Context, skip, take uint64) ([]Product, error) {
+	if take > 100 || (skip == 0 && take == 0) {
+		take = 100
+	}
+	return s.repository.ListProducts(ctx, skip, take)
+}
+func (s *catalogService) ListProductsWithIds(ctx context.Context, ids []string) ([]Product, error) {
+	return s.repository.ListProductsWithIds(ctx, ids)
+}
+func (s *catalogService) SearchProducts(ctx context.Context, query string, skip, take uint64) ([]Product, error) {
+	if take > 100 || (skip == 0 && take == 0) {
+		take = 100
+	}
+	return s.repository.SearchProducts(ctx, query, skip, take)
+}
